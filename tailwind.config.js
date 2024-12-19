43d24fd9df3e2421c9919d2adf75a4a1833053e1
julien drix <juliendrix@MacBook-Pro-de-julien.local>
2024-12-19 16:53:44 +0100

    Fix: Add Tailwind to dependencies or force devDependencies installation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
-module.exports = {
-  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
   },
   plugins: [],
 };
